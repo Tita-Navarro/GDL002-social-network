4e0903786ea912e8b1060fc4307f4e2402434fa6
s5tefany <m.garciac11@gmail.com>
2019-03-22 19:01:19 -0600

    add sendEmailVerification function working by a button

---
+++ src/main.js
@@ -0,0 +1,55 @@
+//Funcion para registrar a los usuarios nuevos
+const registerFunction = () => {
+  var email = document.getElementById('emailRegister').value;
+  var password = document.getElementById('paswordRegister').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+};
+
+document.getElementById('btnRegister').addEventListener('click', () => { registerFunction() });
+
+//Función para verificar por correo electrónico a un usuario
+const emailVerificationFunction =  () => {
+  var user = firebase.auth().currentUser;
+
+  user.sendEmailVerification().then(function() {
+    // Email sent.
+    window.alert('Se envió la verificación')
+  }).catch(function(error) {
+    // An error happened.
+    window.alert('error : ' + error.message);
+  });
+}
+document.getElementById('btnEmailVerification').addEventListener('click', () => { emailVerificationFunction() })
+
+//Funcion para iniciar Sesion
+const loginFunction = () => {
+  var email = document.getElementById('emailLogin').value;
+  var password = document.getElementById('passwordLogin').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+  console.log('Estoy adentro de la sesion');
+
+};
+document.getElementById('btnLogin').addEventListener('click', () => { loginFunction() });
+
+const logoutFunction = () => {
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+  }).catch(function (error) {
+    // An error happened.
+  });
+  console.log('Sali de la sesión');
+}
+
+document.getElementById('btnLogOut').addEventListener('click', () => { logoutFunction() });
