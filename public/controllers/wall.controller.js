67f6b73e7122e3336b0476295509c59e4537bf5e
Tita Navarro <45797993+Tita-Navarro@users.noreply.github.com>
2019-04-03 13:07:36 -0600

    adding functions and style into the social network

--- public/controllers/wall.controller.js
+++ public/controllers/wall.controller.js
@@ -1,45 +1,45 @@
 
 const wallController = (rawTpl, outlet) => {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if(user){
-         const userId= user.uid;
-    const again = () => {
-        document.getElementById('btnLogOut').addEventListener('click', () => logoutFunction());
-        document.getElementById('publishBtn').addEventListener('click', () => createPost());
+
+  const again = () => {
+    document.getElementById('btnLogOut').addEventListener('click', () => logoutFunction());
+    document.getElementById('publishBtn').addEventListener('click', () => createPost());
+    const removeButtons = document.getElementsByClassName('removePost');
+    for (const button of removeButtons) {
+      button.addEventListener('click', (event) => removePost(event)); // creo q aqui puedo insertar el alert
     }
-
-    //Función para Cerrar Sesión
-    const logoutFunction = () => {
-        firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-        }).catch(function (error) {
-        // An error happened.
-        });
-        console.log('Sali de la sesión');
+    const likeButtons = document.getElementsByClassName('likePost');
+    for (const button of likeButtons) {
+      button.addEventListener('click', (event) => countLikes(event));
     }
+    const modButtons = document.getElementsByClassName('modPost');
+      for(const button of modButtons) {
+        button.addEventListener('click', (event)=> modPost(event));
+    }
+  }
 
-    //creacion del post
-    console.log(userId);
+  //Función para Cerrar Sesión
+  const logoutFunction = () => {
+    firebase.auth().signOut().then(function () {
+      // Sign-out successful.
+      console.log('Sali de la sesión');
+    }).catch(function (error) {
+      // An error happened.
+      console.log('Error al intentar salir de la sesión');
+    });
+  }
 
-    const createPost = () => {
-        const postText = document.getElementById('postText').value;
-        // A post entry.
-        var postData = {
-         /*  author: username, */
-          uid: userId,
-          body: postText,
-        /*   title: title,
-          starCount: 0,
-          authorPic: picture */
-        };
-        // Get a key for a new Post.
-        var newPostKey = firebase.database().ref().child('posts').push().key;
-        // Write the new post's data simultaneously in the posts list and the user's post list.
-        var updates = {};
-        updates['/posts/' + newPostKey] = postData;
-        updates['/user-posts/' + userId + '/' + newPostKey] = postData;
-        return firebase.database().ref().update(updates);
+  //creacion del post
+  const createPost = () => {
+    const postText = document.getElementById('postText').value; // obtiene el texto que el usuario ingresó en el input
+    if (postText.length < 4) return; // condicion para que no deje publicar sin contenido // return para que termine ahí y no continue haciendo lo demás :) 
+    firebase.database().ref('/posts').push().set(  //Referencia para acceder a la base de datos para que lleve nuestro postext
+      {
+        text: postText,
+        likes: 0   //para q cree el campo likes
       }
+    );
+  }
 
   // eliminación del post
   const removePost = (event) => {
@@ -47,10 +47,47 @@ const wallController = (rawTpl, outlet) => {
     firebase.database().ref(`/posts/${postId}`).remove();
   }
 
-        outlet.innerHTML = templateEngine(rawTpl, { posts });
-        again();
-    });
-        }
-      });
+  //Contar los likes 
+  const countLikes = (event) => {
+    const likes = parseInt(event.target.dataset.likes, 10) + 1;
+    const postId = event.target.dataset.id;
 
+    firebase.database().ref(`/posts/${postId}`).update({ likes: likes });
+
+  }
+ //Modificar Post - text hi ok
+  const modPost = (event) => {
+  let button = event.target;
+  button.innerHTML = 'Guardar'
+  button.onclick = function () {
+  const postText = document.getElementById('postText').value;
+  const updateText = {text:postText}
+  const postId = event.target.dataset.id;
+  return firebase.database().ref(`/posts/${postId}`).update(updateText);
+  }
+}
+
+
+
+
+
+  //para que aparezca el mas reciente primero
+  firebase.database().ref('/posts/').on('value', function (snapshot) {
+    let posts = snapshot.val();
+    console.log(posts);
+
+    if (!posts) { //Sino existe un post crea un array vacio para que no salga error que esta indefinido
+      posts = []
+    } else {  // 
+      posts = Object.keys(posts)
+        .map(key => {
+          let post = posts[key]; // posts.xbyas56376d23bd76g4
+          post.id = key;
+          return post;
+        })
+        .reverse();
+    }
+    outlet.innerHTML = templateEngine(rawTpl, { posts });
+    again();
+  });
 }
